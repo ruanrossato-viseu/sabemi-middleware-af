bc0f1c3b7e06ee4ba7b9b2fac3366ae11fe4677d
ruanrossato <ruanrossato@gmail.com>
2021-05-21 23:43:47 -0300

    [ADD] Add mock trnscription call

--- app.py
+++ app.py
@@ -6,10 +6,24 @@ Created on Sun Mar  7 12:46:43 2021
 """
 
 import flask
-from flask import request
+from flask import request, make_response
 from botkit import sendToBotkit
 from zenvia import sendToZenvia
+from functools import wraps 
+
 # from mongo import getUserSession
+
+
+def auth_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if auth and auth.username == 'sabemi' and auth.password == '=2VeSDy!v3M!wmwFEXSZ#TPX7dM22WF^s7wkZGXCZuf4MxgP@wrn9UX_3FGZ^*cp':
+            return f(*args, **kwargs)
+
+        return make_response('Could not verify your login!', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+    return decorated
 
 
 app = flask.Flask(__name__)
@@ -36,39 +50,89 @@ def incomingMessage():
 
     replyFromBotkit = sendToBotkit(message,userNumber)
 
-    replyFromZenvia = sendToZenvia(replyFromBotkit,botNumber, userNumber)
+    statusFromZenvia = sendToZenvia(replyFromBotkit,botNumber, userNumber)
     return ""
 
+@app.route('/transcription', methods=['GET'])
+@auth_required
+def getTranscription():
+    return({
+  "datetimeStart": "Wed May 19 2021 13:02:10 GMT-0300",
+  "datetimeFinish": "Wed May 19 2021 13:03:03 GMT-0300",
+  "user": {
+    "id": "123456789",
+    "name": "João da Silva"
+  },
+  "sessionId": "435d1f64-cce2-5ee7-ed23-0be59fe1bde7",
+  "lgpd": "true",
+  "transfered": "false",
+  "convertFinancialAssistance": "true",
+  "convertInsurance": "false",
+  "events": [
+    {
+      "id": "1",
+      "datetime": "Wed May 19 2021 13:02:10 GMT-0300",
+      "type": "createIteraction",
+      "agent": "middleware",
+      "parentId": None
+    },
+    {
+      "id": "2",
+      "datetime": "Wed May 19 2021 13:02:11 GMT-0300",
+      "type": "outgoingMessage",
+      "section": "pre-simulation",
+      "parentId": "1",
+      "agent": "bot",
+      "text": "Olá, João,  eu sou a *Sol*, especialista de Crédito da Sabemi 🙋‍♀️. Tenho uma solução personalizada para você tirar seus planos do papel e realizar seus sonhos"
+    },
+    {
+      "id": "3",
+      "datetime": "Wed May 19 2021 13:02:11 GMT-0300",
+      "type": "outgoingMessage",
+      "section": "pre-simulation",
+      "parentId": "2",
+      "agent": "bot",
+      "text": "Se quiser saber mais, para segurança dos seus dados, preciso garantir que estou falando com a pessoa certa\n\n *João da Silva*, é você mesmo?😊\n\nDigite *Sim*, se for você\n\nDigite *Não*, se você não conhecer essa pessoa"
+    },
+    {
+      "id": "4",
+      "datetime": "Wed May 19 2021 13:02:19 GMT-0300",
+      "type": "incomingMessage",
+      "section": "pre-simulation",
+      "parentId": "3",
+      "agent": "user",
+      "text": "sim"
+    },
+    {
+      "id": "5",
+      "datetime": "Wed May 19 2021 13:02:20 GMT-0300",
+      "type": "outgoingMessage",
+      "section": "pre-simulation",
+      "parentId": "4",
+      "agent": "bot",
+      "text": "Que bom! Agora você esta um passo mais próximo de realizar seus sonhos! 🤩\n\nE para que eu possa apresentar uma proposta na medida, vou precisar que você me informe alguns dos seus dados pessoais.\n\nMas vale ressaltar: *este é um ambiente seguro* e seus dados estão protegidos e guardados, tudo de acordo com a *Lei Geral de Proteção de Dados* (LGPD) e *Direito do Consumidor* 🔒"
+    },
+    {
+      "id": "6",
+      "datetime": "Wed May 19 2021 13:02:20 GMT-0300",
+      "type": "outgoingMessage",
+      "section": "pre-simulation",
+      "parentId": "5",
+      "agent": "bot",
+      "text": "Se quiser saber mais, é só clicar nesse link para acessar nossas políticas e termos sobre a Lei Geral de Proteção de dados: 👉🏼 https://www.sabemi.com.br/politica-de-privacidade"
+    },
+    {
+      "id": "7",
+      "datetime": "Wed May 19 2021 13:02:21 GMT-0300",
+      "type": "outgoingMessage",
+      "section": "pre-simulation",
+      "parentId": "6",
+      "agent": "bot",
+      "text": "Vamos lá!? Me conta qual é o seu *nome completo*?"
+    }
+  ]
+})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=3001)
+    app.run(port=3001)
 
-
-# =============================================================================
-#     {
-#     "id": "191e19ab-b0b2-4baf-b48c-46e23da76a0c",
-#     "timestamp": "2021-04-12T03:52:37.827Z",
-#     "type": "MESSAGE",
-#     "subscriptionId": "cf3e07b1-6892-42b0-98b2-53f68763ed2a",
-#     "channel": "whatsapp",
-#     "direction": "IN",
-#     "message": {
-#         "id": "4b1d75b6-93af-41f7-82a9-aa0ea91a4e0d",
-#         "from": "5511992448799",
-#         "to": "blush-country",
-#         "direction": "IN",
-#         "channel": "whatsapp",
-#         "visitor": {
-#             "name": "Ruan Rossato",
-#             "firstName": "Ruan",
-#             "lastName": "Rossato"
-#         },
-#         "contents": [
-#             {
-#                 "type": "text",
-#                 "text": "asd"
-#             }
-#         ],
-#         "timestamp": "2021-04-12T03:52:36+00:00"
-#     }
-# =============================================================================
